d851f59f896af144978da5c661ee735b5f3e01eb
Daianealexandria <daianefalexandria@gmail.com>
2024-02-28 18:31:31 -0300

    projetovainaweb

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./assets/components/Header/Header";
+
+function  App(){
+return(
+<>
+<h1>Daiane</h1>
+</>
+
+)
+
+}
+export default App
---
+++ src/assets/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header (){
+return(
+<header>
+<h2> Olá, sou a header</h2>
+</header>
+
+
+
+)
+
+}
+
+export default Header
